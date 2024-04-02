f74aa0e038e562e610844abac45a144d3e5e3493
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2024-04-02 18:06:28 -0500

    Add Core App Extension Method

---
+++ PostsService/Core/CoreAppCollections.cs
@@ -0,0 +1,51 @@
+﻿using PostsService.Core.Middlewares;
+
+namespace PostsService.Core
+{
+    public static class CoreAppCollections
+    {
+        public static IApplicationBuilder UseCoreApp(this WebApplication app)
+        {
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+
+
+            //app.UseSerilogRequestLogging(options =>
+            //{
+            //    // Customize the message template   
+            //    options.MessageTemplate = "Request {RequestMethod} {RequestHost}{RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms - From {RemoteIpAddress}";
+
+            //    // Emit debug-level events instead of the defaults
+            //    //options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
+
+            //    //Attach additional properties to the request completion event
+            //    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+            //    {
+            //        diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            //        diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+            //        diagnosticContext.Set("RemoteIpAddress", httpContext.Connection.RemoteIpAddress);
+            //    };
+            //});
+
+
+            return app;
+        }
+
+    }
+}
