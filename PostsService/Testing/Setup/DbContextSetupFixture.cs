18b712e3ec02dbc1a3f0943da097f036e750e7b6
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2024-04-18 18:06:28 -0500

    Setup Database Testing Fixture
    This is to enable testing the Base Repository and With a SQL Lite Fake Connection

---
+++ PostsService/Testing/Setup/DbContextSetupFixture.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using PostsService.Infrastructure.Entities;
+using PostsService.Infrastructure.Repositories;
+
+namespace PostsService.Testing.Setup;
+
+public class DbContextSetupFixture : IDisposable
+{
+
+    private static string DefaultSqLiteMemoryConnectionString = $"Data Source={Guid.NewGuid()};Mode=Memory;";
+    private static SqliteConnection _connection { get; set; } = null!;
+    public static TestDbContext CreateContextForSQLiteInMemory(string? connectionString = null)
+    {
+        if (string.IsNullOrWhiteSpace(connectionString)) { connectionString = DefaultSqLiteMemoryConnectionString; }
+        _connection = new SqliteConnection(connectionString);
+        _connection.Open();
+        var _options = new DbContextOptionsBuilder()
+                .UseSqlite(_connection)
+                .Options;
+        var context = new TestDbContext(_options);
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        return context;
+    }
+
+    public void Dispose()
+    {
+        if (_connection != null)
+        {
+            _connection.Dispose();
+        }
+    }
+}
+public class TestDbContext : DbContext
+{
+    public TestDbContext(DbContextOptions options) : base(options) { }
+    public DbSet<RepositoryTestEntity> RepositoryTestEntities { get; set; }
+}
+
+public class RepositoryTestEntity : IEntity
+{
+    public required string Name { get; set; }
+}
+public class TestRepository : Repository<RepositoryTestEntity, TestDbContext>
+{
+    public TestRepository() : base(DbContextSetupFixture.CreateContextForSQLiteInMemory())
+    {
+    }
+
+}
